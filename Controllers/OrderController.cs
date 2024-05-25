6c476ae10fe375dbfb220d80cc887bc3ec3f4cb0
Flash <albert821107@gmail.com>
2024-05-25 22:22:08 +0800

    feat:建立查詢單筆Order API及新增Dapper套件

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,5 +1,52 @@
-﻿namespace Sample_AP.Controllers;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Sample_AP.Model;
 
-public class OrderController
+namespace Sample_AP.Controllers;
+
+[ApiController]
+[Route("api/")]
+public class OrderController : ControllerBase
 {
+    // 連線字串
+    private readonly string _connectionString = "data source=.;initial catalog=Northwind;persist security info=True;Trusted_Connection=True;TrustServerCertificate=true;";
+
+    public OrderController() { }
+
+    [HttpGet]
+    [Route("order/{orderID}")]
+    public IActionResult GetOrderByID(int orderID)
+    {
+        string sql = @"
+            SELECT *
+            FROM Orders
+            WHERE OrderID = @OrderID";
+
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            var order = connection.QuerySingleOrDefault<Order>(sql, new { OrderID = orderID });
+
+            return Ok(order);
+        }
+    }
+
+    [HttpDelete]
+    [Route("order/{orderID}")]
+    public IActionResult DeleteOrderByID(int orderID)
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            var parameters = new { OrderID = orderID };
+
+            // 使用 Dapper 調用預存程序
+            connection.Execute("DeleteOrderDataBYID", parameters, commandType: System.Data.CommandType.StoredProcedure);
+
+            return Ok("完成刪除資料");
+        }
+    }
 }
---
+++ Model/Order.cs
@@ -0,0 +1,52 @@
+﻿namespace Sample_AP.Model;
+
+public class Order
+{
+    public Order() { }
+
+    public Order(
+    int orderID,
+    string customerID,
+    int employeeID,
+    DateTime orderDate,
+    DateTime requiredDate,
+    DateTime? shippedDate,
+    int shipVia,
+    decimal freight,
+    string shipName,
+    string shipAddress,
+    string shipCity,
+    string shipRegion,
+    string shipPostalCode,
+    string shipCountry)
+    {
+        OrderID = orderID;
+        CustomerID = customerID;
+        EmployeeID = employeeID;
+        OrderDate = orderDate;
+        RequiredDate = requiredDate;
+        ShippedDate = shippedDate;
+        ShipVia = shipVia;
+        Freight = freight;
+        ShipName = shipName;
+        ShipAddress = shipAddress;
+        ShipCity = shipCity;
+        ShipRegion = shipRegion;
+        ShipPostalCode = shipPostalCode;
+        ShipCountry = shipCountry;
+    }
+    public int OrderID { get; set; }
+    public string CustomerID { get; set; }
+    public int EmployeeID { get; set; }
+    public DateTime OrderDate { get; set; }
+    public DateTime RequiredDate { get; set; }
+    public DateTime? ShippedDate { get; set; }
+    public int ShipVia { get; set; }
+    public decimal Freight { get; set; }
+    public string ShipName { get; set; }
+    public string ShipAddress { get; set; }
+    public string ShipCity { get; set; }
+    public string ShipRegion { get; set; }
+    public string ShipPostalCode { get; set; }
+    public string ShipCountry { get; set; }
+}
