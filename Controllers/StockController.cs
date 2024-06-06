7d53cb7aa87b859b7e71539f5f229d861fd87bb1
Flash <albert821107@gmail.com>
2024-06-06 23:07:52 +0800

    'feat:新增獲取單支上市股票資訊API'

---
+++ Controllers/StockController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sample_AP.Controllers;
+
+[ApiController]
+[Route("api/")]
+public class StockController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+
+    public StockController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    [HttpGet]
+    [Route("Stock/{stockID}")]
+    public async Task<IActionResult> GetStockByID(string stockID)
+    {
+        string resultURL = $"https://mis.twse.com.tw/stock/api/getStockInfo.jsp?json=1&delay=0&ex_ch=tse_{stockID}.tw";
+
+        HttpResponseMessage response = await _httpClient.GetAsync(resultURL);
+
+        response.EnsureSuccessStatusCode();
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        return Ok(responseBody);
+    }
+}
